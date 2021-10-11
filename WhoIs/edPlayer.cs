fa4c131caa82ff4568e66665619f4a8eecc6aa5b
Artem <artmedia70@gmail.com>
2021-10-12 05:16:13 +0700

    Почистил, класс событий журналадобавил

--- WhoIs/edPlayer.cs
+++ WhoIs/edPlayer.cs
@@ -72,10 +72,11 @@ namespace WhoIs
             this.Name(name); 
         }
         // Методы установки/получения значений переменных
-        public  string      Name()                                  { return this.name;         }
-        public  void        Name(string name)                       { this.name = name;         }
-        public  DateTime    TimeCreate()                            { return this.time_create;  }
-        public  DateTime    TimeLast()                              { return this.time_last;    }
+        public  List<SquadronData> SquadronsList()                  { return this.squadrons_list;   }
+        public  string      Name()                                  { return this.name;             }
+        public  void        Name(string name)                       { this.name = name;             }
+        public  DateTime    TimeCreate()                            { return this.time_create;      }
+        public  DateTime    TimeLast()                              { return this.time_last;        }
         public  void        TimeCreate(string time)
         { 
             this.time_create = DateTime.Parse(time, null, DateTimeStyles.AdjustToUniversal);
@@ -90,6 +91,12 @@ namespace WhoIs
             SquadronData squadron = this.squadrons_list.ElementAt(i);
             return squadron != null ? squadron.Name() : "";
         }
+        
+        /// <summary>
+        /// Добавляет в список новую эскадру (имя эскадры, дата и время вступления)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="time"></param>
         public  void        AddNewSquadron(string name, string time)
         {
             SquadronData squadron = new SquadronData(name, time);
@@ -149,10 +156,60 @@ namespace WhoIs
         }
     }
 
+    public enum ENUM_JOURNAL_EVENT
+    {
+        JOURNAL_EVENT_CREATE_NEW_PILOT,         // Создание нового пилота
+        JOURNAL_EVENT_SQUADRON_ENTRY,           // Присоединение к эскадре
+        JOURNAL_EVENT_SQUADRON_LEAVE,           // Выход из эскадры
+        JOURNAL_EVENT_GAME_MODE_PRIVATE,        // Режим игры "Приватная группа"
+        JOURNAL_EVENT_GAME_MODE_SOLO,           // Режим игры "Соло"
+        JOURNAL_EVENT_GAME_ENTRY_WRONG_NAME,    // Вход в игру под другим именем
+    };
+
+
+    public class JournalEvent
+    {
+        private ENUM_JOURNAL_EVENT  jevent;
+        private DateTime            time;
+        private string              subject;
+        public JournalEvent() { }
+        public JournalEvent(ENUM_JOURNAL_EVENT journal_event, string time_event, string subject)
+        {
+            this.jevent = journal_event;
+            this.time = DateTime.Parse(time_event, null, DateTimeStyles.AdjustToUniversal);
+            this.subject = subject;
+        }
+        
+        public string   TimeAsString()  { return this.time.ToString();  }
+        public DateTime Time()          { return this.time;             }
+        public string   Subject()       { return this.subject;          }
+        
+        public string EventDescription(ENUM_JOURNAL_EVENT jevent)
+        {
+            switch(jevent)
+            {
+                case ENUM_JOURNAL_EVENT.JOURNAL_EVENT_CREATE_NEW_PILOT      : return $"{this.TimeAsString()} Создание нового пилота: \"{this.Subject()}\"";
+                case ENUM_JOURNAL_EVENT.JOURNAL_EVENT_SQUADRON_ENTRY        : return $"{this.TimeAsString()} Присоединение к эскадре: \"{this.Subject()}\"";
+                case ENUM_JOURNAL_EVENT.JOURNAL_EVENT_SQUADRON_LEAVE        : return $"{this.TimeAsString()} Выход из эскадры: \"{this.Subject()}\"";
+                case ENUM_JOURNAL_EVENT.JOURNAL_EVENT_GAME_MODE_PRIVATE     : return $"{this.TimeAsString()} Режим игры \"Приватная группа\"";
+                case ENUM_JOURNAL_EVENT.JOURNAL_EVENT_GAME_MODE_SOLO        : return $"{this.TimeAsString()} Режим игры \"Соло\"";
+                case ENUM_JOURNAL_EVENT.JOURNAL_EVENT_GAME_ENTRY_WRONG_NAME : return $"{this.TimeAsString()} Вход в игру под другим именем: \"{this.Subject()}\"";
+                default:
+                break;
+            }
+            return "Unknown";
+        }
+    }
+
+
+    /// <summary>
+    /// Класс "Игрок" описывает игрока - имена его пилотов, присутствие в эскадрах
+    /// </summary>
     public class edPlayer
     {
-        private List<PilotData> pilots_list = new List<PilotData>(10);
-        private List<LogFile>   log_files_list = new List<LogFile>();
+        private List<PilotData>     pilots_list = new List<PilotData>(10);
+        private List<LogFile>       log_files_list = new List<LogFile>();
+        private List<JournalEvent>  events_list = new List<JournalEvent>();
         // Конструкторы
         public edPlayer() 
         { 
@@ -180,27 +237,91 @@ namespace WhoIs
             return true;
         }
 
+        /// <summary>
+        /// Возвращает по индексу эскадру указанного по индексу пилота
+        /// </summary>
+        /// <param name="pilot_index"></param>
+        /// <param name="squadron_index"></param>
+        /// <returns></returns>
+        public string PilotSquadron(int pilot_index, int squadron_index)
+        {
+            PilotData pilot=this.pilots_list.ElementAt(pilot_index);
+            if(pilot is null)
+                return "";
+            return pilot.Squadron(squadron_index);
+            return "";
+        }
+
+        /// <summary>
+        /// Возвращает список эскадр пилота по его имени
+        /// </summary>
+        /// <param name="pilot_name"></param>
+        /// <returns></returns>
+        public List<SquadronData> SquadronList(string pilot_name)
+        {
+            PilotData pilot=this.PilotByName(pilot_name);
+            if(pilot == null)
+                return null;
+            return pilot.SquadronsList();
+        }
+
+        /// <summary>
+        /// Добавляет к списку пилотов игрока новое имя пилота
+        /// </summary>
+        /// <param name="pilot"></param>
         protected void AddNewPilot(PilotData pilot)
         {
             if(!this.pilots_list.Contains(pilot))
                 this.pilots_list.Add(pilot);
         }
-        public int PilotsCount() { return this.pilots_list.Count; }
-        public PilotData Pilot() { return this.pilots_list.ElementAt(0); }
-        public PilotData Pilot(int index) 
+        /// <summary>
+        /// Возвращает объект "Пилот" по индексу из списка пилотов игрока
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public PilotData Pilot(int index=0) 
         {
             if(this.pilots_list.Count == 0)
                 return null;
             return this.pilots_list.ElementAt(index < 0 ? 0 : index > pilots_list.Count-1 ? pilots_list.Count-1 : index); 
         }
-        public List<LogFile> GetListLogFiles() { return this.log_files_list; }
 
+        /// <summary>
+        /// Возвращает объект "Пилот" по имени пилота
+        /// </summary>
+        /// <param name="pilot_name"></param>
+        /// <returns></returns>
+        public PilotData PilotByName(string pilot_name)
+        {
+            foreach(PilotData pilot in this.pilots_list)
+            {
+                if(pilot.Name() == pilot_name)
+                    return pilot;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Возвращает количество пилотов игрока
+        /// </summary>
+        /// <returns></returns>
+        public int PilotsCount() { return this.pilots_list.Count; }
+
+        
+        public void History(string path_to_logs)
+        {
+            if(this.CollectLogFilesAll(path_to_logs))
+            {
+
+            }
+        }
+        
         /// <summary>
         /// Создаёт список всех лог-файлов игры за всё время (Альфа не учитывается)
         /// </summary>
         /// <param name="path_to_logs"></param>
         /// <returns></returns>
-        public bool CollectLogFilesAll(string path_to_logs)
+        private bool CollectLogFilesAll(string path_to_logs)
         {
             //список всех файлов с расширением log
             string[] array = Directory.GetFiles(path_to_logs, "*.log");
@@ -211,13 +332,19 @@ namespace WhoIs
             }
             return this.log_files_list.Count>0;
         }
+        
+        /// <summary>
+        /// Возвращает список всех лог-файлов игрока
+        /// </summary>
+        /// <returns></returns>
+        public List<LogFile> GetListLogFiles() { return this.log_files_list; }
 
         /// <summary>
         /// Возвращает из строки имя пилота
         /// </summary>
         /// <param name="line"></param>
         /// <returns></returns>
-        public string PilotName(string line)
+        private string PilotName(string line)
         {
             int beg = line.IndexOf("Commander")+12;
             int end = line.IndexOf(",", beg);
@@ -228,7 +355,7 @@ namespace WhoIs
         /// </summary>
         /// <param name="line"></param>
         /// <returns></returns>
-        public DateTime EventTime(string line)
+        private DateTime EventTime(string line)
         {
             int beg = line.IndexOf("timestamp")+12;
             int end = line.IndexOf("Z",beg);
@@ -240,7 +367,7 @@ namespace WhoIs
         /// </summary>
         /// <param name="line"></param>
         /// <returns></returns>
-        public string SquadronName(string line)
+        private string SquadronName(string line)
         {
             int beg = line.IndexOf("SquadronName")+15;
             int end = line.IndexOf(",", beg);
